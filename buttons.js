6971c3e2c4ee181bc223194fde319ffbf091a672
Vasanth Velusamy <vasanthv16@gmail.com>
2025-08-30 19:29:31 -0400

    selecting dropdown shows enchantment details

--- buttons.js
+++ buttons.js
@@ -1,5 +1,43 @@
+let enchantmentsDiv = document.getElementById("enchantments");
+let dropdownElement = document.getElementById("target-item-selector");
+
+
+
+dropdownElement.innerHTML = "<option value=''>Choose your enchantment</option>";
+
 for (let i = 0; i < items.length; i++) {
     let item = items[i];
-    document.getElementById("target-item-selector").innerHTML +=
-        "<option>" + item.name + "</option>";
+    dropdownElement.innerHTML +=
+        "<option value='" + item.name + "'>" + item.name + "</option>";
 }
+
+function createEnchantment(enchantment, enchantmentNumber) {
+    let maxLevel = enchantment["max-level"];
+    enchantmentsDiv.innerHTML += enchantment.name;
+    for (let j = 0; j < maxLevel; j++) {
+
+        enchantmentsDiv.innerHTML +=
+            `<input type='radio' name='group-number-${enchantmentNumber}' id='1.${j}' value='${enchantmentNumber}.${j + 1}'/>${j}`;
+    }
+    enchantmentsDiv.innerHTML += "<br/>"
+}
+
+
+
+
+function onItemSelected(itemName) {
+
+    let selectedItem = items.find(item => item.name == itemName);
+    enchantmentsDiv.innerHTML = "";
+
+    if (selectedItem) {
+        for (let k = 0; k < selectedItem.enchantments.length; k++) {
+            let enchantment = selectedItem.enchantments[k];
+            createEnchantment(enchantment, k);
+        }
+    }
+
+}
+
+
+
